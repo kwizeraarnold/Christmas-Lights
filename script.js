df269e7e957f6023d401540b3600aecc434fc81b
kwizeraarnold <33444720+kwizeraarnold@users.noreply.github.com>
2021-06-09 14:04:41 +0300

    change intensity feature

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 let on = document.querySelector('#on')
 let off = document.querySelector('#off')
+let speedInput = document.querySelector("#sInput")
+let speedButton = document.querySelector("#sButton")
 let arr = ['one-off', 'two-off', 'three-off', 'four-off', 'five-off', 'six-off', 'seven-off']
 let arr1 = ['one', 'two', 'three', 'four', 'five', 'six', 'seven']
 let list = document.querySelectorAll("li")
+
+console.log(speedInput)
+
+function changeSpeed() {
+    let input = speedInput.value + 's'
+    for (let i = 0; i < list.length; i++) {
+        list[i].style["animationDuration"] = input
+    }
+}
 
 function turnLightsOff(){
     for (let i = 0; i < list.length; i++) {
@@ -18,5 +29,6 @@ function turnLightsOn(){
 
 off.addEventListener('click', turnLightsOff)
 on.addEventListener('click', turnLightsOn)
+speedButton.addEventListener('click', changeSpeed)
 
 
